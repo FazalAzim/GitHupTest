c6ab1041446d8fa6fa20ccbbe93c263d21f9d14a
Fazal <fazalazim238@gmail.com>
2023-02-07 12:26:04 +0500

    remove triple promise

--- programming.js
+++ programming.js
@@ -407,28 +407,3 @@
 // console.log(promise);
 
 // ****************************************************************
-
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('Welcome');
-//   }, 1000);
-// });
-// console.log(promise);
-
-// ****************************************************************
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('Welcome');
-//   }, 1000);
-// });
-// console.log(promise);
-
-// ****************************************************************
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('Welcome');
-//   }, 1000);
-// });
-// console.log(promise);
-
-// ****************************************************************
